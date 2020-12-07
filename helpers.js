d11c35a0892c7376751b34a8ddd233b33a12a0b2
Jakkha23 <jakob.khaytan@skola.botkyrka.se>
2020-12-07 14:21:32 +0100

    added 'swap' function

--- helpers.js
+++ helpers.js
@@ -15,3 +15,18 @@ export function randomArray(len, n) {
 function randomInt(n) {
     return Math.floor(Math.random() * (n + 1))
 }
+
+    const array1 =[5, 8, 10, 9]
+    const array2 =[1, 2, 3, 4]
+    const array3 =[5, 6, 7, 8]
+
+export function swap(arr, arr1, arr2) {
+    let temp = arr[arr1]
+    arr[arr1] = arr[arr2]
+    arr[arr2] = temp
+    console.log(arr)
+}
+
+swap(array3, 1, 3)
+swap(array1, 1, 3)
+swap(array2, 1, 3)
